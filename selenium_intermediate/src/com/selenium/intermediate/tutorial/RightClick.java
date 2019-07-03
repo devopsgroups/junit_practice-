82279ec92399c927e66f171ebd4d5dfd23d05261
manish <manish.tiwari@infostretch.com>
2019-07-03 23:11:58 +0530

    right click

--- selenium_intermediate/src/com/selenium/intermediate/tutorial/RightClick.java
+++ selenium_intermediate/src/com/selenium/intermediate/tutorial/RightClick.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class RightClickExample {
+public class RightClick {
 
 	public static void main(String[] args) {
 		
